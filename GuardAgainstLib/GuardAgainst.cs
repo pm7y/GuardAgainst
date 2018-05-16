4fa722c788f0a0e900ec83930c63061304469d18
Paul Mcilreavy <paul@mcilreavy.com>
2018-05-16 14:24:00 +1000

    Added DebuggerNonUserCode attribute.

--- GuardAgainstLib/GuardAgainst.cs
+++ GuardAgainstLib/GuardAgainst.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace GuardAgainstLib
 {
+    [DebuggerNonUserCode]
     public static class GuardAgainst
     {
         #region Null, Whitespace, Empty
